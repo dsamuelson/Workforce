6cad9ccd72d35d30b7e652f3e90535820f8fbf15
David Samuelson <dsamuelson89@gmail.com>
2022-03-27 15:27:00 -0600

    added tests for engineer object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,40 @@
-const { jsxNamespacedName } = require('@babel/types');
-const { test, expect } = require('@jest/globals');
+const { jest:requiredJest, test, expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 
-test('creates employee object', () => {
+describe('Tests if the employee object is created successfully', () => {
+    test('creates employee object', () => {
     const employee = new Employee('John',1234,'jimmy@james.com');
 
     expect(employee.name).toBe('John');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-})
+    expect(employee.role).toBe('Employee');
+});
+});
+
+describe('Checks if methods are returned as Employee', () => {
+test('checks employee role method', () => {
+    const employee = new Employee('john', 1234, 'jimy@james.com');
+
+    expect(employee.getRole()).toBe('Employee');
+});
+
+test('checks employee Name method', () => {
+    const employee = new Employee('John',1234,'jimmy@james.com');
+
+    expect(employee.getName()).toBe('John');
+});
+
+test('Checks employee id method', () => {
+    const employee = new Employee('John',1234,'jimmy@james.com');
+
+    expect(employee.getId()).toEqual(employee.id.toString());
+});
+
+test('checks employee email method', () => {
+    const employee = new Employee('John',1234,'jimmy@james.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining('jimmy@james.com'));
+});
+
+});
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,45 @@
+const { TestWatcher } = require('jest');
 const Engineer = require('../lib/Engineer');
+
+describe('Tests if the engineer object is created successfully', () => {
+    test('Checks fields for engineer object', () => {
+        const engineer = new Engineer('John', 1234, 'John@john.com', 'Engineer');
+
+        expect(engineer.name).toBe('John');
+        expect(engineer.id).toEqual(expect.any(Number));
+        expect(engineer.email).toEqual(expect.any(String));
+        expect(engineer.role).toEqual('Engineer')
+    });
+});
+
+describe('Tests if Employee methods return succesfully', () => {
+    test('Tests getName method', () => {
+        const engineer = new Engineer('John', 1234, 'John@john.com', 'Engineer');
+
+        expect(engineer.getName()).toBe('John');
+    });
+    test('Tests getId method', () => {
+        const engineer = new Engineer('John', 1234, 'John@john.com', 'Engineer');
+
+        expect(engineer.getId()).toEqual(engineer.id.toString());
+    });
+
+    test('Tests getEmail method', () => {
+        const engineer = new Engineer('John', 1234, 'John@john.com', 'Engineer');
+
+        expect(engineer.getEmail()).toBe('John@john.com');
+    });
+    test('Tests getRole method', () => {
+        const engineer = new Engineer('John', 1234, 'John@john.com', 'Engineer');
+
+        expect(engineer.getRole()).toBe('Engineer');
+    });
+});
+
+describe('Tests Engineer specific method', () => {
+    test('Checks if github link is returned', () => {
+        const engineer = new Engineer('John', 1234, 'John@john.com', 'Engineer', 'dsamuelson');
+
+    expect(engineer.getGithub()).toBe('https://github.com/dsamuelson?tab=repositories')
+});
+});
