8ec10a9364c9ab9bf449136ac2a73b05cc343e11
David Samuelson <dsamuelson89@gmail.com>
2022-03-27 15:51:22 -0600

    added tests for manager and intern

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,45 @@
 const Intern = require('../lib/Intern');
+
+describe('Tests if the intern object is created successfully', () => {
+    test('Checks fields for intern object', () => {
+        const intern = new Intern('John', 1234, 'John@john.com', 'Intern', 'Notre Dame');
+
+        expect(intern.name).toBe('John');
+        expect(intern.id).toEqual(expect.any(Number));
+        expect(intern.email).toEqual(expect.any(String));
+        expect(intern.role).toEqual('Intern')
+        expect(intern.school).toBe('Notre Dame');
+    });
+});
+
+describe('Tests if Employee methods return succesfully', () => {
+    test('Tests getName method', () => {
+        const intern = new Intern('John', 1234, 'John@john.com', 'Intern');
+
+        expect(intern.getName()).toBe('John');
+    });
+    test('Tests getId method', () => {
+        const intern = new Intern('John', 1234, 'John@john.com', 'Intern');
+
+        expect(intern.getId()).toEqual(intern.id.toString());
+    });
+
+    test('Tests getEmail method', () => {
+        const intern = new Intern('John', 1234, 'John@john.com', 'Intern');
+
+        expect(intern.getEmail()).toBe('John@john.com');
+    });
+    test('Tests getRole method', () => {
+        const intern = new Intern('John', 1234, 'John@john.com', 'Intern');
+
+        expect(intern.getRole()).toBe('Intern');
+    });
+});
+
+describe('Tests Intern specific method', () => {
+    test('Checks if school is returned', () => {
+        const intern = new Intern('John', 1234, 'John@john.com', 'Intern', 'Notre Dame');
+
+    expect(intern.getSchool()).toBe('Notre Dame');
+});
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,45 @@
 const Manager = require('../lib/Manager');
+
+describe('Tests if the manager object is created successfully', () => {
+    test('Checks fields for manager object', () => {
+        const manager = new Manager('John', 1234, 'John@john.com', 'Manager', 4567);
+
+        expect(manager.name).toBe('John');
+        expect(manager.id).toEqual(expect.any(Number));
+        expect(manager.email).toEqual(expect.any(String));
+        expect(manager.role).toEqual('Manager')
+        expect(manager.officeNum).toEqual(expect.any(Number));
+    });
+});
+
+describe('Tests if Employee methods return succesfully', () => {
+    test('Tests getName method', () => {
+        const manager = new Manager('John', 1234, 'John@john.com', 'Manager');
+
+        expect(manager.getName()).toBe('John');
+    });
+    test('Tests getId method', () => {
+        const manager = new Manager('John', 1234, 'John@john.com', 'Manager');
+
+        expect(manager.getId()).toEqual(manager.id.toString());
+    });
+
+    test('Tests getEmail method', () => {
+        const manager = new Manager('John', 1234, 'John@john.com', 'Manager');
+
+        expect(manager.getEmail()).toBe('John@john.com');
+    });
+    test('Tests getRole method', () => {
+        const manager = new Manager('John', 1234, 'John@john.com', 'Manager');
+
+        expect(manager.getRole()).toBe('Manager');
+    });
+});
+
+describe('Tests Manager specific method', () => {
+    test('Checks if manager Role link is returned', () => {
+        const manager = new Manager('John', 1234, 'John@john.com', 'Manager');
+
+    expect(manager.getRole()).toBe('Manager')
+});
+});
